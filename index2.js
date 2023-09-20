63a67aa8cfac3f62a032147844e9398c1a2a418c
TheCoderAishwarya <113344237+TheCoderAish@users.noreply.github.com>
2023-09-21 01:23:08 +0530

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,2 @@
+let label = document.querySelector('label');
+label.innerHTML = label.innerText.split('').map((letters, i) => `<span style="transition-delay: ${i * 30}ms;filter: hue-rotate(${i * 10}deg);">${letters}</span>`).join('')
